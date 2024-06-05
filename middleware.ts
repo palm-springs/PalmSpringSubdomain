f6d72b0fbec026cce5b33db644c59796541630a5
seobbang <seobbangway@naver.com>
2024-06-05 14:47:54 +0900

    RELEASE v1.10.1 - feat: 서브 도메인 robots.txt 경로 루트 도메인 robots.txt로 리다이렉트

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,8 @@ export const middleware = (request: NextRequest) => {
     return NextResponse.redirect(
       new URL(`${HTTP_PROTOCOL}://${subdomain}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}`, request.url),
     );
+  } else if (pathName === '/robots.txt') {
+    return NextResponse.redirect(new URL(`https:/${process.env.NEXT_PUBLIC_DOMAIN_NAME}/sitemap.xml`, request.url));
   } else {
     console.log('logging subdomain', host);
     if (host === 'srookie.palmsummer.site') {
