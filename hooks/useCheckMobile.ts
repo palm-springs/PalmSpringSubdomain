23236c69ee96831bfe374d865f54ed93d0c3218e
seobbang <seobbangway@naver.com>
2024-06-15 17:20:58 +0900

    feat/#472: set initial value with device type

--- hooks/useCheckMobile.ts
+++ hooks/useCheckMobile.ts
@@ -3,12 +3,14 @@
 import { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
-const useCheckMobile = () => {
-  const [isMobile, setIsMobile] = useState(false);
+// 서버 측에서 계산한 isMobile 값을 initialValue로 설정
+const useCheckMobile = (initialValue: boolean) => {
+  const [isMobile, setIsMobile] = useState(initialValue);
 
   const mobile = useMediaQuery({
     query: '(max-width:768px)',
   });
+
   useEffect(() => {
     if (mobile) {
       setIsMobile(mobile);
