38f14866a83a111bad5697f49a8a4675f9dce9f2
seobbang <seobbangway@naver.com>
2024-06-15 17:20:08 +0900

    feat/#472: check device type in middleware

--- middleware.ts
+++ middleware.ts
@@ -7,18 +7,26 @@ export const middleware = (request: NextRequest) => {
   const hostArray = request.headers.get('host')?.split('.');
   const subdomain = hostArray?.[0];
   const pathName = request.nextUrl.clone().pathname;
+
+  // User-Agent를 사용하여 디바이스 모바일 여부 결정
+  const userAgent = request.headers.get('user-agent');
+  const isMobile = userAgent ? /mobile/i.test(userAgent) : false;
+
   if (pathName.startsWith('/home')) {
     return NextResponse.redirect(
       new URL(`${HTTP_PROTOCOL}://${subdomain}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}`, request.url),
     );
   } else if (pathName === '/robots.txt') {
-    return NextResponse.redirect(new URL(`https:/${process.env.NEXT_PUBLIC_DOMAIN_NAME}/robots.txt`, request.url));
+    return NextResponse.redirect(new URL(`https://${process.env.NEXT_PUBLIC_DOMAIN_NAME}/robots.txt`, request.url));
   } else {
     console.log('logging subdomain', host);
     if (host === 'srookie.palmsummer.site') {
       return NextResponse.rewrite(new URL(`/official${request.nextUrl.clone().pathname}`, request.url));
     }
-    return NextResponse.rewrite(new URL(`/${subdomain}${request.nextUrl.clone().pathname}`, request.url));
+    // 모바일 여부 헤더 세팅
+    const response = NextResponse.rewrite(new URL(`/${subdomain}${request.nextUrl.clone().pathname}`, request.url));
+    response.headers.set('x-is-mobile', isMobile.toString());
+    return response;
   }
 };
 
