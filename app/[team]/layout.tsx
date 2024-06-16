19c335d1e6f5fe44c0e832b9a6bf89bf3ed609f8
seobbang <seobbangway@naver.com>
2024-06-16 19:10:31 +0900

    feat/#472: header, footer 모바일 깜빡임 현상 해결

--- app/[team]/layout.tsx
+++ app/[team]/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { headers } from 'next/headers';
 
 import { getBlogFooterInfo, getBlogHeaderInfo } from '@/apis/blogHome';
 import BlogFooter from '@/components/common/BlogFooter';
@@ -23,11 +24,14 @@ const BlogHomeLayout = async ({ children, params }: { children: React.ReactEleme
     data: { ownerName, ownerInfo },
   } = blogFooterInfoRes;
 
+  const headerList = headers();
+  const isDeviceMobile = headerList.get('x-is-mobile') === 'true';
+
   return (
     <>
-      <BlogHeader logo={logo} blogName={blogName} navList={navList} />
+      <BlogHeader logo={logo} blogName={blogName} navList={navList} isDeviceMobile={isDeviceMobile} />
       <main style={{ overflowX: 'hidden' }}>{children}</main>
-      <BlogFooter companyName={ownerName} companyDetail={ownerInfo} />
+      <BlogFooter companyName={ownerName} companyDetail={ownerInfo} isDeviceMobile={isDeviceMobile} />
     </>
   );
 };
