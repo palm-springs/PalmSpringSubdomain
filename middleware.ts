9f9cc0b3bee655d32286b5a319f2238c4c167c97
seobbang <seobbangway@naver.com>
2024-06-18 18:51:04 +0900

    RELEASE - v1.11.4

--- middleware.ts
+++ middleware.ts
@@ -12,9 +12,6 @@ export const middleware = (request: NextRequest) => {
   const userAgent = request.headers.get('user-agent');
   const isMobile = userAgent ? /mobile/i.test(userAgent) : false;
 
-  console.log('User-Agent:', userAgent); // 로그 추가
-  console.log('Is Mobile:', isMobile); // 로그 추가
-
   if (pathName.startsWith('/home')) {
     return NextResponse.redirect(
       new URL(`${HTTP_PROTOCOL}://${subdomain}.${process.env.NEXT_PUBLIC_DOMAIN_NAME}`, request.url),
@@ -23,12 +20,8 @@ export const middleware = (request: NextRequest) => {
     return NextResponse.redirect(new URL(`https://${process.env.NEXT_PUBLIC_DOMAIN_NAME}/robots.txt`, request.url));
   } else {
     console.log('logging subdomain', host);
-    if (host === 'srookie.palmsummer.site') {
-      return NextResponse.rewrite(new URL(`/official${request.nextUrl.clone().pathname}`, request.url));
-    }
     // 모바일 여부 헤더 세팅
     const response = NextResponse.rewrite(new URL(`/${subdomain}${request.nextUrl.clone().pathname}`, request.url));
-    response.headers.set('user-agent', userAgent.toString());
     response.headers.set('x-is-mobile', isMobile.toString());
     return response;
   }
