ed327c5cdc4db9bfd8e5acff7072793ceb68f80f
9911120 <psalm1266@gmail.com>
2024-07-08 01:34:13 +0900

    feat/#27: GA 연동 흔적 지우기

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'styled-components';
 
@@ -11,8 +10,6 @@ import StyledComponentsRegistry from '@/utils/lib/registry';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID!;
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <body className={inter.className}>
@@ -20,7 +17,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <ThemeProvider theme={theme}>
           <GlobalStyle />
           {children}
-          {process.env.NODE_ENV !== 'development' && <GoogleAnalytics gaId={GA_ID} />}
           <div id="modal-root"></div>
         </ThemeProvider>
       </StyledComponentsRegistry>
